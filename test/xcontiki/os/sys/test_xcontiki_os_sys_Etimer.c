15ac3fd58bcf841100bf697f5cbeb6f9f555be85
jarojuda <jarojuda@gmail.com>
2021-05-16 11:18:39 +0200

    Etimer unit test skeleton.

---
+++ test/xcontiki/os/sys/test_xcontiki_os_sys_Etimer.c
@@ -0,0 +1,32 @@
+#include "unity.h"
+#include "xcontiki/xcontiki.h"
+
+
+void setUp(void){
+
+}
+
+void tearDown(void){
+
+}
+
+void test_when_EtimerIsSetFirstTime_thenShouldAllocateNonZeroIndeks(void){
+
+
+  TEST_FAIL_MESSAGE("Define Etimer Test");
+
+}
+
+void test_when_EtimerIsAllocated_thenItShouldNotChangeIndeks(void){
+
+
+  TEST_FAIL_MESSAGE("Define Etimer Test");
+
+}
+
+void test_when_EtimerIsExpired_thenItShouldGenerateEvent(void){
+
+
+  TEST_FAIL_MESSAGE("Define Etimer Test");
+
+}
