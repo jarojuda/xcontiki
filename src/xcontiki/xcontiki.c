e8574b7740a3c118c5534dee49be582c4e24582f
Jaroslaw Juda <jarojuda@gmail.com>
2021-09-03 14:29:34 +0200

    Add the protothread swcheduler in the xcontiki main loop for hardware threads.

--- src/xcontiki/xcontiki.c
+++ src/xcontiki/xcontiki.c
@@ -1,12 +1,32 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2020, Jaroslaw Juda <mail at JaroslawJuda.site>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 /* Based on the Contiki http://contiki-os.org
  * and the Contiki-NG  http://www.contiki-ng.org/
- * projects. 
+ * projects.
  */
 
 /* 
@@ -28,6 +48,7 @@ xcontiki_os_sys_Protothread__state_t xcontiki__main(void) {
     }
 
     xcontiki_arch_dev_Watchdog__periodic();
+    xcontiki_arch_dev__threads();
     xcontiki_os_sys_Etimer__request_poll();
     if (xcontiki_os_sys_Process__number_of_events_waiting() > 0) {
         xcontiki_os_sys_Process__process_next_event();
@@ -44,7 +65,7 @@ void xcontiki__init(void) {
 }
 
 static void init(void) {
-    xcontiki_arch_dev_Watchdog__init();
+    xcontiki_arch_dev__init();
     xcontiki_arch_dev_Watchdog__start();
     xcontiki_arch_Clock__init();
     xcontiki_os_sys_Process__initialize_the_process_module();
